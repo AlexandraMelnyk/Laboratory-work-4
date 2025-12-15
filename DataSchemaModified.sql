a78201a91a1529477c5a2f4237c98ad0bf11a804
AlexandraMelnyk <167908850+AlexandraMelnyk@users.noreply.github.com>
2025-12-15 19:03:18 +0200

    Update DataSchemaModified.sql

--- DataSchemaModified.sql
+++ DataSchemaModified.sql
@@ -1,20 +1,24 @@
 CREATE TABLE users (
-    email VARCHAR(50),
+    user_email VARCHAR(50),
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     user_password VARCHAR(100),
-    CONSTRAINT users_pk PRIMARY KEY (email),
-    CONSTRAINT users_email_unique UNIQUE (email),
+    CONSTRAINT users_pk PRIMARY KEY (user_email),
+    CONSTRAINT users_email_unique UNIQUE (user_email),
     CONSTRAINT user_email_template
-        CHECK (email ~ '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'),
+        CHECK (
+            user_email ~ '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$'
+        ),
     CONSTRAINT username_format
-        CHECK (first_name ~ '^[A-Za-z]+$')
+        CHECK (
+            first_name ~ '^[A-Za-z]+$'
+        )
 );
 
 CREATE TABLE sports_event (
     event_title VARCHAR(50),
     event_date DATE,
-    location VARCHAR(100),
+    event_location VARCHAR(100),
     CONSTRAINT events_pk PRIMARY KEY (event_title)
 );
 
@@ -24,36 +28,42 @@ CREATE TABLE event_registration (
     event_title VARCHAR(50),
     CONSTRAINT reg_pk PRIMARY KEY (user_email, event_title),
     CONSTRAINT reg_user_fk
-        FOREIGN KEY (user_email) REFERENCES users (email),
+        FOREIGN KEY (user_email)
+            REFERENCES users (user_email),
     CONSTRAINT reg_event_fk
-        FOREIGN KEY (event_title) REFERENCES sports_event (event_title)
+        FOREIGN KEY (event_title)
+            REFERENCES sports_event (event_title)
 );
 
 CREATE TABLE payment (
     payment_date DATE,
-    amount NUMERIC(10, 2),
+    payment_amount NUMERIC(10, 2),
     user_email VARCHAR(50),
     event_title VARCHAR(50),
     CONSTRAINT payment_reg_fk
         FOREIGN KEY (user_email, event_title)
-        REFERENCES event_registration (user_email, event_title)
+            REFERENCES event_registration (user_email, event_title)
 );
 
 CREATE TABLE restaurant (
-    name VARCHAR(50),
-    rating NUMERIC(2, 1),
-    distance NUMERIC(5, 1),
+    restaurant_name VARCHAR(50),
+    rating_value NUMERIC(2, 1),
+    distance_km NUMERIC(5, 1),
     event_title VARCHAR(50),
     CONSTRAINT restaurant_event_fk
-        FOREIGN KEY (event_title) REFERENCES sports_event (event_title),
+        FOREIGN KEY (event_title)
+            REFERENCES sports_event (event_title),
     CONSTRAINT rating_range
-        CHECK (rating BETWEEN 0 AND 5)
+        CHECK (
+            rating_value BETWEEN 0 AND 5
+        )
 );
 
 CREATE TABLE notification (
-    message VARCHAR(255),
+    notification_text VARCHAR(255),
     send_date DATE,
     user_email VARCHAR(50),
     CONSTRAINT notif_user_fk
-        FOREIGN KEY (user_email) REFERENCES users (email)
+        FOREIGN KEY (user_email)
+            REFERENCES users (user_email)
 );
